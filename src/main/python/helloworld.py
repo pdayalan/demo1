4f21eb7087a35741468974f18f6fe3989fa8ece7
pdayalan <pdayalan@gmail.com>
2024-03-13 22:56:12 +0530

    "update3"

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -11,7 +11,7 @@ def helloworld(out):
     Args:
         out: Output stream to write the message to.
     """
-    out.write("Hello world of Python\n")
+    out.write("Hello world of javaPython\n")
 
 # Call the function with a file object
 helloworld(sys.stdout)
